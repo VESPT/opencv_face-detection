bb9e26c2d16124cb79b7c27b573ef9e33889f236
VESPT <vesp1989@gmail.com>
2016-05-10 14:49:45 +0900

    first commit

---
+++ openCVSamplefacedetection/build/generated/source/buildConfig/fat/debug/org/opencv/samples/facedetect/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.opencv.samples.facedetect;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.opencv.samples.facedetect";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "fat";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final int VALUE = 1;
+}
